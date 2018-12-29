d9f2cb0fd863cfb06be2f214db59b9386aabb151
Chad Wade Day, Jr <cdayjr@users.noreply.github.com>
2018-12-28 16:45:49 -0800

    Modified directory structure, added more pnpm scripts, prepared for adding to npm repo, fixed some typos, added tsdoc comments.

--- src/video-tag.ts
+++ src/video-tag.ts
@@ -1,9 +1,20 @@
-import Video from "./video.ts";
+import Video from './video.ts';
 
+/**
+ * Grab the video tags to parse from the HTML page.
+ *
+ * @return An HTMLDivElement array.
+ */
 const getVideoTags = (): HTMLDivElement[] => {
-  return document.querySelectorAll("div.video-tag:not([data-options])");
+  return document.querySelectorAll('div.video-tag:not([data-options])');
 };
 
+/**
+ * Parse a video tag and fill it with the video embed code.
+ * Note- its contents will be cleared.
+ *
+ * @param A video tag.
+ */
 const parseVideoTag = (tag: HTMLDivElement): void => {
   if (undefined !== tag.dataset.content) {
     if (undefined !== tag.dataset.source) {
@@ -19,13 +30,16 @@ const parseVideoTag = (tag: HTMLDivElement): void => {
     tag.dataset.options = video.exportOptions();
   } else {
     // Leave no options, this is an invalid tag.
-    tag.dataset.source = "Invalid";
-    tag.dataset.options = "";
+    tag.dataset.source = 'Invalid';
+    tag.dataset.options = '';
   }
 };
 
+/**
+ * Find all the video tags on the page and parse each of them.
+ */
 const parseVideoTags = (): void => {
   getVideoTags().forEach(parseVideoTag);
 };
 
-document.addEventListener("DOMContentLoaded", parseVideoTags);
+document.addEventListener('DOMContentLoaded', parseVideoTags);
