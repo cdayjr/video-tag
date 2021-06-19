d0f661a00c52428d42129058dc67ce98dac3c9b7
Chad Wade Day, Jr <cdayjr@users.noreply.github.com>
2021-06-19 15:30:59 -0700

    load eslint config properly

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const yaml = require("yaml");
 
-const file = fs.readFileSync("./eslint.yaml", "utf8");
+const file = fs.readFileSync(__dirname + "/eslint.yaml", "utf8");
 
 const eslintConfig = yaml.parse(file);
 
